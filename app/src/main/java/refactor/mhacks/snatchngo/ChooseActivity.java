3d4cd62f2466b8bd381b6eefda75b340faed54cf
DChang87 <diana.chang.twn@gmail.com>
2016-02-20 08:00:22 -0500

    added base of firebase

--- app/src/main/java/refactor/mhacks/snatchngo/ChooseActivity.java
+++ app/src/main/java/refactor/mhacks/snatchngo/ChooseActivity.java
@@ -7,6 +7,8 @@ import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 
+import com.firebase.client.Firebase;
+
 /**
  * Created by abc96_000 on 2016-02-20.
  */
@@ -15,7 +17,7 @@ public class ChooseActivity extends ActionBarActivity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_activity);
-
+        Firebase.setAndroidContext(this);
 
 
     }
